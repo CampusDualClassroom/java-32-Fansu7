fbc534cfe980437079be1b828c50e8eeffd3a1d9
Alfonso Vidal <vidaltorres.alfonso@gmail.com>
2025-02-13 09:38:07 +0100

    Resolver ejercicio32

--- src/main/java/com/campusdual/classroom/Exercise32.java
+++ src/main/java/com/campusdual/classroom/Exercise32.java
@@ -3,32 +3,42 @@ package com.campusdual.classroom;
 import com.campusdual.util.Utils;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+
 public class Exercise32 {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    printToFile(generateStringToSave(generateUserInputToSave()));
+  }
 
+  public static String generateStringToSave(String string) {
+    if (string == null) {
+      return generateUserInputToSave();
+    } else {
+      return string;
     }
+  }
 
-    public static String generateStringToSave(String string) {
-
+  private static String generateUserInputToSave() {
+    StringBuilder sb = new StringBuilder();
+    System.out.println("Escribe debajo el texto que quieras. Pulsa \"ENTER\" 2 veces seguidas para finalizar:");
+    String string;
+    while (!(string = Utils.string()).isEmpty()) {
+      sb.append(string).append(System.lineSeparator());
     }
+    return sb.toString();
+  }
 
-    private static String generateUserInputToSave(){
-        StringBuilder sb = new StringBuilder();
-        System.out.println("Escribe debajo el texto que quieras. Pulsa \"ENTER\" 2 veces seguidas para finalizar:");
-        String string;
-        while(!(string = Utils.string()).isEmpty()){
-            sb.append(string).append(System.lineSeparator());
-        }
-        return sb.toString();
+  public static void printToFile(String string) {
+
+    try (BufferedWriter bw = new BufferedWriter(new PrintWriter(new File("src/main/resources/data.txt")))) {
+      bw.write(string);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
-
-    public static void printToFile(String string) {
-
-    }
-
+  }
 
 }
